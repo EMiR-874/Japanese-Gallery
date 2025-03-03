332c4c00c38d7a697b9e3756164c62e923afafc6
EMiR-874 <emir.hr.uni@gmail.com>
2025-03-03 22:16:31 +0100

    Add files via upload

---
+++ a-tags/tjn1/tjn1-script.js
@@ -0,0 +1,30 @@
+function toggleMenu() {
+  const menuIcon = document.getElementById("toggle_menu_icon");
+  menuIcon.classList.toggle("clicked");
+
+  document.querySelector(".menu_contact_info").classList.toggle("hidden-contact");
+  document.querySelector(".body_main_content").classList.toggle("body_content_hidden");
+  document.querySelector(".footer").classList.toggle("body_content_hidden");
+}
+
+document.getElementById("toggle_menu_icon").addEventListener("click", toggleMenu);
+document.getElementById("toggle_menu_text").addEventListener("click", toggleMenu);
+
+document.querySelector(".logo_positioner").addEventListener("click", function () {
+  window.location.href = "/index.html";
+});
+
+//PREV
+document.querySelector(".prev").addEventListener("click", function () {
+  window.location.href = "/a-tags/tja1/tja1.html";
+});
+
+//CENTER
+document.querySelector(".center").addEventListener("click", function () {
+  window.location.href = "/index.html";
+});
+
+//NEXT
+document.querySelector(".next").addEventListener("click", function () {
+  window.location.href = "/a-tags/tjf2/tjf2.html";
+});
